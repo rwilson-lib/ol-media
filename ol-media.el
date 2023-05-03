a6e890568974b39cb4e2c7e7632663f1bc211da6
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2023-05-03 23:40:26 +0000

    added copyright and author docstr

--- ol-media.el
+++ ol-media.el
@@ -1,8 +1,37 @@
-;;; ol-medie.el - Support for link links to in Org mode
+;;; ol-media.el --- Org-mode support for media link   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Ramus Jabee Lloyd Wilson
+
+;; Author: Ramus Jabee Lloyd Wilson
+;; URL: "https://github.com/rwilson-lib/ol-media"
+
+;; Keywords: files, outlines, hypermedia
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.1") (mpv "0.35.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; org-mode links to media files
 
 ;;; Code:
 
 (require 'ol)
+(eval-when-compile
+  (require 'subr-x))
+
 (require 'mpv)
 
 (defvar org-media-link-keymap (make-sparse-keymap))
